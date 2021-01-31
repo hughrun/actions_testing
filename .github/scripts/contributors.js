36db3ce0816674e245021c0e87da476942e6baee
Hugh Rundle <hugh@hughrundle.net>
2021-01-31 16:23:40 +1100

    initial commit for add-contributors

---
+++ .github/scripts/contributors.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const cheerio = require('cheerio');
+const pretty = require('pretty');
+const commits = process.env.COMMITS
+const $ = cheerio.load(page)
+var page = fs.readFileSync('./contributors.html', {encoding: 'utf-8' })
+var contributors = fs.readFileSync('./contributors.txt', {encoding: 'utf-8' }).split('\n')
+
+$('#contributors').empty() // remove all names to start clean
+for (let name of contributors) {
+  if (name.length > 0) {
+    for (let commit of commits) {
+      if ( !contributors.includes(commit.author.name) ) {
+        contributors.push(commit.author.name)
+      }
+    } // TODO: add filter to ignore bots
+    $('#contributors').append(`<li>${name}</li>\n`)
+  }
+}
+
+// write out new contributors.html file
+let newHtml = pretty($.html(), {ocd: true})
+fs.writeFileSync('./contributors.html', newHtml, {encoding: 'utf-8'})
+// write out contributors.md
+let newTxt = "";
+contributors.map( x => {
+  if (x) {
+    newTxt = newTxt.concat(`${x}\n`)
+  }
+})
+
+fs.writeFileSync('./contributors.txt', newTxt, {encoding: 'utf-8'})
