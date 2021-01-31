b1ed86b96345601221b434e7912c8ec1ab49b85a
Hugh Rundle <hugh@hughrundle.net>
2021-01-31 17:23:27 +1100

    Update contributors.js
    remove logging

--- .github/scripts/contributors.js
+++ .github/scripts/contributors.js
@@ -6,7 +6,6 @@ var page = fs.readFileSync('./contributors.html', {encoding: 'utf-8' })
 var contributors = fs.readFileSync('./contributors.txt', {encoding: 'utf-8' }).split('\n')
 const $ = cheerio.load(page)
 
-console.log(process.env.GITHUB_EVENT_PATH)
 var eventJson = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf-8' }));
 
 $('#contributors').empty() // remove all names to start clean
