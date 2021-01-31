1fe994c4d371978401f2a95764a3e96e7e3bc459
Hugh Rundle <hugh@hughrundle.net>
2021-01-31 17:20:51 +1100

    Update contributors.js
    read event from file

--- .github/scripts/contributors.js
+++ .github/scripts/contributors.js
@@ -7,32 +7,27 @@ var contributors = fs.readFileSync('./contributors.txt', {encoding: 'utf-8' }).s
 const $ = cheerio.load(page)
 
 console.log(process.env.GITHUB_EVENT_PATH)
-var eventJson = fs.readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf-8' })
-console.log(typeof eventJson);
-console.log(eventJson);
+var eventJson = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf-8' }));
 
-axios.get(process.env.GITHUB_EVENT_PATH)
-.then( event => {
-  $('#contributors').empty() // remove all names to start clean
-  for (let name of contributors) {
-    if (name.length > 0) {
-      for (let commit of event.commits) {
-        if ( !contributors.includes(commit.author.name) ) {
-          contributors.push(commit.author.name)
-        }
-      } // TODO: add filter to ignore bots
-      $('#contributors').append(`<li>${name}</li>\n`)
-    }
+$('#contributors').empty() // remove all names to start clean
+for (let name of contributors) {
+  if (name.length > 0) {
+    for (let commit of eventJson.commits) {
+      if ( !contributors.includes(commit.author.name) ) {
+        contributors.push(commit.author.name)
+      }
+    } // TODO: add filter to ignore bots
+    $('#contributors').append(`<li>${name}</li>\n`)
   }
-  // write out new contributors.html file
-  let newHtml = pretty($.html(), {ocd: true})
-  fs.writeFileSync('./contributors.html', newHtml, {encoding: 'utf-8'})
-  // write out contributors.md
-  let newTxt = "";
-  contributors.map( x => {
-    if (x) {
-      newTxt = newTxt.concat(`${x}\n`)
-    }
-  })
-  fs.writeFileSync('./contributors.txt', newTxt, {encoding: 'utf-8'})
+}
+// write out new contributors.html file
+let newHtml = pretty($.html(), {ocd: true})
+fs.writeFileSync('./contributors.html', newHtml, {encoding: 'utf-8'})
+// write out contributors.md
+let newTxt = "";
+contributors.map( x => {
+  if (x) {
+    newTxt = newTxt.concat(`${x}\n`)
+  }
 })
+fs.writeFileSync('./contributors.txt', newTxt, {encoding: 'utf-8'})
